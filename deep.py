df7144ba1291a56ca5e39d5e329e29a8277f301b
test <test>
2023-12-23 15:48:49 +0700

    test

---
+++ deep.py
@@ -0,0 +1,5 @@
+answer = input("Какой ответ на главный вопрос жизни, вселенной и всего такого? "). lower() .strip()
+if answer == "42" or answer == "сорок два":
+    print ("Да")
+else:
+    print ("Нет")
