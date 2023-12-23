f4f1b93d5419038d2aa0107f507b6f0d5b0f4631
test <test>
2023-12-23 16:08:14 +0700

    test

---
+++ calories.py
@@ -0,0 +1,15 @@
+fruits = {
+    "apple": 130,
+    "lime": 20
+    }
+
+fruit = input("Фрукт: ")
+
+if fruit.lower() in fruits:
+    calories = fruits[fruit.lower()]
+    print(f"Калории: {calories}")
+elif fruit.lower() in fruits:
+    calories = fruits[fruit.lower()]
+    print(f"Калории: {calories}")
+else:
+    print
